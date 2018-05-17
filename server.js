30cf254f42ae91a2d20788feaecd98247bcdadf1
tjandy98 <tjandy98@gmail.com>
2018-05-17 23:18:00 +0800

    Queries are now stored in database. Minor index page updates

--- server.js
+++ server.js
@@ -4,6 +4,18 @@ const http = require('http');
 const socketIO = require('socket.io');
 const path = require('path');
 
+//firebase
+var admin = require("firebase-admin");
+var serviceAccount = require('./serviceAccountKey.json');
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://askaboutblockchain.firebaseio.com"
+});
+
+var db = admin.database();
+var ref = db.ref("questions");
+
+
 const {generateMessage} = require('./public/js/message');
 const qna = require('./qna');
 
@@ -34,14 +46,24 @@ app.get('/', (req, res) => {
       qna.get_answers(qna.method, content, function (result) {
         // Write out the response from the /knowledgebases/create method.
             let obj = JSON.parse(result)
+            let replyMessage = "";
             if (obj.answers[0].answer == 'No good match found in KB.'){
-              socket.emit('newMessage', generateMessage('Bot', "I'm sorry, I am not trained to answer that yet."));
+              replyMessage = "I'm sorry, I am not trained to answer that yet.";
+              socket.emit('newMessage', generateMessage('Bot', replyMessage));
+              
             }
             else{
+              replyMessage = obj.answers[0].answer;
               socket.emit('newMessage', generateMessage('Bot', obj.answers[0].answer));
             }
             
            
+      var newPostRef = ref.push();
+      newPostRef.set({
+        question: message.text,
+        answer: obj.answers[0].answer
+        
+      });
         });
 
       callback();
@@ -51,6 +73,16 @@ app.get('/', (req, res) => {
   
    
   });
+
+// Initialize Firebase
+// TODO: Replace with your project's customized code snippet
+// var config = {
+//   apiKey: "AIzaSyA89ULimpSQzMmPBALey4a1rYWaPwJx2RY",
+//   authDomain: "askaboutblockchain.firebaseapp.com",
+//   databaseURL: "https://askaboutblockchain.firebaseio.com",
+//   storageBucket: "askaboutblockchain.appspot.com",
+// };
+
 
 
 
